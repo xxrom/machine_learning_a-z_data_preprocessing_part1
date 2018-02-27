f648062ea7fbd9e6707d5d86d7793f52bde3a09f
Nikita Chernyshov <xpom55@mail.ru>
2018-02-27 08:57:57 +0300

    p2l17, comment Taking care of missing data

--- my_data_p_t.py
+++ my_data_p_t.py
@@ -5,25 +5,26 @@ import pandas as pd
 
 # importin the database
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+X = dataset.iloc[:, :-1].values # копируем все кроме последнего столбца
+y = dataset.iloc[:, 3].values # копируем только последний столбец
 
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
-imputer = Imputer(
+imputer = Imputer( # ищем пустые поля и заполняем из срендними значениями
   missing_values = 'NaN',
   strategy = 'mean',
   axis = 0
 )
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+imputer = imputer.fit(X[:, 1:3]) #? прокидываем 2,3 и 4 столбцы
+X[:, 1:3] = imputer.transform(X[:, 1:3]) #? вставляем данные в X
 
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 lavelencoder_X = LabelEncoder()
-X[:, 0] = lavelencoder_X.fit_transform(X[:, 0])
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
+X[:, 0] = lavelencoder_X.fit_transform(X[:, 0]) # пререводим страны в цифры
+onehotencoder = OneHotEncoder(categorical_features = [0]) # настраиваем переводчик
+X = onehotencoder.fit_transform(X).toarray() # по первому столбцу делаем разные катеогрии
+# т.е. было [1, 2, 3] а будет три стобца [[1,00],[010],[001]]
 
 lavelencoder_y2 = LabelEncoder()
 y = lavelencoder_y2.fit_transform(y)
@@ -41,7 +42,8 @@ X_train, X_test, y_train, y_test = train_test_split(
 # test_size = 0.2 - соотношение 0.8(80%) - обучение и 0.2(20%) - тестирование
 
 
-# Feature Scaling
+# Feature Scaling приводим все цифры к интервалу -1 до 1, смотря какой способ
+# так быстрее будет работать обучение и не будет перевеса у данных разных
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
@@ -51,7 +53,7 @@ X_test = sc_X.transform(X_test)
 
 
 
-
+60*8 + 120*2 + 90*8 +30*8
 
 
 
